d851ecfa1b4dbaf9aa550558dcdb577baae480bd
Zhanylmyrza <bkrvj83@gmail.com>
2022-07-01 22:53:35 +0600

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '73028c5d-6635-49aa-9ed5-7934ee9ba97a';
+export const MODAL_CLOSE_SEC = 2.5;
